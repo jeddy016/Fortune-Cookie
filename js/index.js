2839fd8d51d0b94afdd26233a9ca27989c09cb61
Jordan Eddy <jeddy016@gmail.com>
2017-01-24 20:19:47 -0600

    Fix memory leak

--- js/index.js
+++ js/index.js
@@ -1,24 +1,32 @@
-$(document).ready(function() {
-//Get new Fortune//
-  $('.btn').click(function getFortune() {
-    $('.header').addClass('fortune');
-    $.ajax({
-      url: 'https://fortunecookieapi.herokuapp.com/v1/cookie',
+
+var fortunes = [];
+
+function getFortune() {
+  $.ajax({
+      url: 'http://fortunecookieapi.herokuapp.com/v1/fortunes',
+      cache: false,
       success: function(data) {
-        var fort = JSON.stringify(data[0].fortune.message);
-//Only return fortunes short enough for Tweet//
-        fort.length <= 125 ? $('.header').text(fort) :
-          getFortune();
-      }
-    })
-  });
-//Pre-fill tweet with fortune//
-  $('.tweet').click(function() {
-//Format fortune for URL//
+        fortunes.push(data);
+    }
+  })
+}
+
+function postTweet() {
+//Format fortune for URL
     var preFill = $('.header').text()
       .replace(/["/.]/g, "")
       .replace(/\s/g, "%20");
-//Insert into URL//
-    $('.tweet').attr('href', 'https://twitter.com/intent/tweet?text=My%20fortune%20is%3A%20' + '%22' + preFill + '%22');
+//Insert into URL
+    $('.tweet').attr('href', 'https://twitter.com/intent/tweet?text=My%20FortuneCookie%20fortune%20is%3A%20' + '%22' + preFill + '%22');
+  };  
+
+
+$(document).ready(function() {
+  getFortune()
+  $('.btn').click(function() {
+    $('.header').addClass('fortune')
+      .text(JSON.stringify(fortunes[0][Math.floor((Math.random() * 100) + 1)]
+      .message));
+    postTweet();
   });
 });
